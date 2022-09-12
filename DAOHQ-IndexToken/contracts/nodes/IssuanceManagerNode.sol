fab70e6b0b6799f86f5d6436d33a296fa0eafd44
ianrowan <i.rowan299@gmail.com>
2022-09-12 11:46:24 -0500

    management fee and dynamic valuation

--- DAOHQ-IndexToken/contracts/nodes/IssuanceManagerNode.sol
+++ DAOHQ-IndexToken/contracts/nodes/IssuanceManagerNode.sol
@@ -17,41 +17,25 @@ interface IUniswapV2Router02{
     function getAmountsOut(uint amountIn, address[] calldata path) external view returns (uint[] memory amounts);
 }
 
+interface ImanagementFeeNode{
+    function getFeesPending(IToken indexToken) external view returns(uint256[] memory);
+}
 
 contract IssuanceManager is DaoHqRouter{
 
     uint256 startPrice; //in wei ETH
-    IToken public indexToken;
     //comment out for prod
     IUniswapV2Router02 UniswapRouter = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
+    ImanagementFeeNode feeNode;
     address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-    constructor(address _indexToken, uint256 _startPrice) {
-        indexToken = IToken(_indexToken);
+
+    constructor(uint256 _startPrice, address feeManager) {
         startPrice = _startPrice;
+        feeNode = ImanagementFeeNode(feeManager);
     }
 
-    /*function _executeswap(address component, uint256 cumulativeShare, bytes32[] calldata paths) private {
-        uint256 share = indexToken.getShare(component);
-
-        uint256 amountOut = hqswap(IERC20(address(0)), value, 0, paths);
-
-        indexToken.updateHoldings(component, amountOut, true);
-
-        IERC20(component).transfer(address(indexToken), amountOut);
-    }
-
-    function _executeSwaptoETH(address component, uint256 indexQty, bytes32[] calldata paths) private returns(uint256 amountOut){
-        // % of supply/ownership of index * balance of given token 
-        uint256 amountIn = (indexQty/indexToken.totalSupply()) * IERC20(component).balanceOf(address(indexToken));
-        
-        indexToken.approve(address(this), amountIn);
-        amountOut = hqswap(IERC20(component), amountIn, 0, paths);
-
-        indexToken.updateHoldings(component, amountOut, false);
-    }*/
-
-    function _executeswap(address component, uint256 cumulativeShare, uint256 msgVal) private returns(uint256[] memory amountOut) {
+    function _executeswap(address component, uint256 cumulativeShare, uint256 msgVal, IToken indexToken) private returns(uint256[] memory amountOut) {
         uint256 share = indexToken.getShare(component);
         uint256 value = (msgVal * share) / cumulativeShare;
 
@@ -62,7 +46,7 @@ contract IssuanceManager is DaoHqRouter{
         amountOut = UniswapRouter.swapExactETHForTokens{value: value}(1, path, address(indexToken), block.timestamp);   
     }
 
-    function _executeSwaptoETH(address component, uint256 indexQty) private returns(uint256 amountOut){
+    function _executeSwaptoETH(address component, uint256 indexQty, IToken indexToken) private returns(uint256 amountOut){
 
         uint256 amountIn = IERC20(component).balanceOf(address(indexToken));
         // 0 index qty signals an exit
@@ -83,41 +67,41 @@ contract IssuanceManager is DaoHqRouter{
         amountOut = UniswapRouter.swapExactTokensForETH(amountIn, 0, path, address(this), block.timestamp)[1];
     }
 
-    function _swapEthForAll(uint256 ethVal) private {
-        (address[] memory components, uint256 cumulativeShare) = _getComponentsShare();
+    function _swapEthForAll(IToken indexToken, uint256 ethVal) private {
+        (address[] memory components, uint256 cumulativeShare) = _getComponentsShare(indexToken);
         //Buy each component
         for(uint i = 0; i<components.length; i++){
-            _executeswap(components[i], cumulativeShare, ethVal/*, paths[i]*/);
+            _executeswap(components[i], cumulativeShare, ethVal, indexToken/*, paths[i]*/);
         }
     }
 
-    function _exit(address component) private returns (uint256 amountOut){
-        return _executeSwaptoETH(component, 0);
+    function _exit(address component, IToken indexToken) private returns (uint256 amountOut){
+        return _executeSwaptoETH(component, 0, indexToken);
     }
 
-    function _getComponentsShare() private view returns(address[] memory components, uint256 cumulativeShare){
+    function _getComponentsShare(IToken indexToken) private view returns(address[] memory components, uint256 cumulativeShare){
         components = indexToken.getComponents();
         cumulativeShare = indexToken.getCumulativeShare();
     }
 
-    function issueForExactETH(uint minQty, address to /*, bytes32[][] calldata paths*/) external payable {
+    function issueForExactETH(IToken indexToken, uint minQty, address to /*, bytes32[][] calldata paths*/) external payable {
         uint256 preSupply = indexToken.totalSupply();
-        uint256 preValue = valueSet();
+        uint256 preValue = valueSet(indexToken);
         uint256 outputTokens = (msg.value * preSupply) / preValue;
         require(outputTokens >= minQty, "Insuffiecient return amount");
 
-        _swapEthForAll(msg.value);
-        require((preValue + (preSupply + outputTokens))/preSupply == valueSet(), "set misbalanced");
+        _swapEthForAll(indexToken, msg.value);
+        require((preValue + (preSupply + outputTokens))/preSupply == valueSet(indexToken), "set misbalanced");
         indexToken.mint(to, outputTokens);
     }
 
-    function redeem(uint qty, address to /*, bytes32[][] calldata paths*/) external {
+    function redeem(IToken indexToken, uint qty, address to /*, bytes32[][] calldata paths*/) external {
         require(indexToken.balanceOf(to) >= qty, "User does not have sufficeint balance");
-        uint expectedOut = (qty * valueSet()) / indexToken.totalSupply();
-        (address[] memory components, ) = _getComponentsShare();
+        uint expectedOut = (qty * valueSet(indexToken)) / indexToken.totalSupply();
+        (address[] memory components, ) = _getComponentsShare(indexToken);
         uint256 funds = 0;
         for(uint i = 0; i<components.length; i++){
-            funds += _executeSwaptoETH(components[i], qty /*, paths[i]*/);
+            funds += _executeSwaptoETH(components[i], qty, indexToken /*, paths[i]*/);
         }
         require(expectedOut == funds, "incorrect redemption amount");
 
@@ -126,27 +110,29 @@ contract IssuanceManager is DaoHqRouter{
         require(sent, "Failed to Transfer");
     }
 
-    function rebalanceExitedFunds(address[] memory exitedPositions) external {
+    function rebalanceExitedFunds(IToken indexToken, address[] memory exitedPositions) external {
         uint preBalance = address(this).balance;
         {
             for(uint i = 0; i < exitedPositions.length; i++){
                 address component = exitedPositions[i];
                 require(indexToken.getShare(component) == 0, "position not exited");
-                _exit(component);
+                _exit(component, indexToken);
                 require(IERC20(component).balanceOf(address(indexToken)) == 0 &&
                 IERC20(component).balanceOf(address(this)) == 0, "Token not exited properly");
             }
         }
 
         uint postBalance = address(this).balance;
-        _swapEthForAll(postBalance - preBalance);
+        _swapEthForAll(indexToken, postBalance - preBalance);
     }
     
-    function valueSet() public view returns (uint256 wethValue){
+    function valueSet(IToken indexToken) public view returns (uint256 wethValue){
         wethValue = 0;
         address[] memory components = indexToken.getComponents();
+        uint256[] memory pendingFees = feeNode.getFeesPending(indexToken);
         for (uint i = 0; i < components.length; i++){
             uint256 bal = IERC20(components[i]).balanceOf(address(indexToken));
+            bal -= pendingFees[i];
             address[] memory path = new address[](2);
             path[0] = components[i];
             path[1] = WETH;
